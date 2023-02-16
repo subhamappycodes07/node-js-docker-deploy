9be9f2eb6e84f2536cbd0ce8b06febab408b1d19
subhamappycodes07 <subham.haldar@appycodes.com>
2023-02-16 18:38:58 +0530

    implementing docker compose

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,4 @@
 const info = (req, res) => {
-    res.status(200).send("Docker deployment test 1");
+    res.status(200).send("Docker deployment using docker-compose");
 };
 module.exports = { info };
