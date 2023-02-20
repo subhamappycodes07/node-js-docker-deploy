426a966743a419b254768a6ef48734b583f5b8a1
subhamappycodes07 <subham.haldar@appycodes.com>
2023-02-20 17:24:22 +0530

    connection between multiple container

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 const express = require('express');
+const mongoose = require("mongoose");
+const { MONGO_USER, MONGO_PASS, MONGO_IP, MONGO_PORT } = require('./config/config');
 const { userRouter } = require('./routes/userRoutes');
 const app = express()
 const port = process.env.PORT || 5000;
 
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASS}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`
+
+const connectWithRetry = () => {
+    mongoose.connect(mongoURL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }).then(() => console.log("connected to DB")).catch((err) => {
+        console.log(err)
+        setTimeout(connectWithRetry, 5000)
+    })
+}
+
+
+connectWithRetry()
 
 
 require('dotenv').config()
