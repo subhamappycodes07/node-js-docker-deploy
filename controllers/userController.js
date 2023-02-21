d74862d384ca8e6e16c681f6019f1d8241fe4220
subhamappycodes07 <subham.haldar@appycodes.com>
2023-02-21 16:15:37 +0530

    changes on userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,4 @@
 const info = (req, res) => {
-    res.status(200).send({ msg: "Multiple container connection" });
+    res.status(200).send({ msg: "Docker image deploy to ec2" });
 };
 module.exports = { info };
