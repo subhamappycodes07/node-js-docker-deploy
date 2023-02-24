58357f7668f7ea85bb2814753e2a37577ae2df8c
subhamappycodes07 <subham.haldar@appycodes.com>
2023-02-24 14:20:34 +0530

    problrm founded

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,4 @@
 const info = (req, res) => {
-    res.status(200).send({ msg: "Docker image deploy to ec2" });
+    res.status(200).send({ msg: "Docker image deploy to ec23" });
 };
 module.exports = { info };
